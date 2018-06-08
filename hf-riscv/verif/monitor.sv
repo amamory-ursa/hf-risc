0d21e622681087bacb2313b0671012113ac07d59
Atila Romero <atilaromero@gmail.com>
2018-06-08 15:10:52 -0300

    termination_monitor to monitor.sv

--- hf-riscv/verif/monitor.sv
+++ hf-riscv/verif/monitor.sv
@@ -8,16 +8,21 @@ class Monitor_cbs;
   endtask
 endclass
 
+typedef class Termination_monitor;
+
 class monitor;
    virtual hfrv_interface.monitor iface;
    event   terminated;
    Monitor_cbs cbs[$];
+   Termination_monitor termination_monitor;
    mailbox msgout;
 
    function new(virtual hfrv_interface.monitor iface, input event terminated, mailbox msgout);
       this.iface = iface;
       this.terminated = terminated;
       this.msgout = msgout;
+      this.termination_monitor = new(this.terminated);
+      this.cbs.push_back(this.termination_monitor);
    endfunction // new
 
    task run();
@@ -35,4 +40,21 @@ class monitor;
    endtask
    
 endclass // monitor
+
+class Termination_monitor extends Monitor_cbs;
+  event terminated;
+
+  function new(ref event terminated);
+    this.terminated = terminated;
+  endfunction
+
+  virtual task mem(virtual hfrv_interface.monitor iface, mailbox msgout);
+    if (iface.mem.address == 32'he0000000 && iface.mem.data_we != 4'h0)
+    begin
+      iface.mem.data_read <= {32{1'b0}};
+      ->terminated;
+    end
+  endtask
+
+endclass
 `endif
