332b67d77d1f2d5a2d35ab14ddbeb0eefd197f9a
Atila Romero <atilaromero@gmail.com>
2018-06-13 15:24:55 -0300

    WIP cover_opcodes

--- hf-riscv/verif/callbacks/monitor/cover_opcodes.sv
+++ hf-riscv/verif/callbacks/monitor/cover_opcodes.sv
@@ -32,22 +32,25 @@ class CoverOpCodes_cbs extends Monitor_cbs;
     Snapshot snap;
     super.time_step(t, timemachine);
 
+    if (!timemachine.isInstruction(t))
+      return;
     snap = timemachine.snapshot[t];
     assert(snap.base[1:0]==2'b11) else
     begin
-      $display("Error: base[1:0] != 2'b11 : %2b", base[1:0]);
+      $display("Error: base[1:0] != 2'b11 : %2b", snap.base[1:0]);
       this.nErrors++;
     end
-    assert($cast(opcode, base[6:0])) else
+    assert($cast(opcode, snap.base[6:0])) else
     begin
-      $display("Error: opcode not expected: %7b", base[6:0]);
-      $display("base: %32b", base[31:0]);
+      $display("Error: opcode not expected: %7b", snap.base[6:0]);
+      $display("base: %32b", snap.base);
+      $display("data_read: %32b", snap.data_read);
       this.nErrors++;
     end
-    if ($cast(opcode, base[6:0])) begin
-      assert($cast(base, base & OpcodeMask[opcode])) else
+    if ($cast(opcode, snap.base[6:0])) begin
+      assert($cast(snap.base, snap.base & OpcodeMask[opcode])) else
       begin
-        $display("Error: intruction not expected: %32b", base);
+        $display("Error: intruction not expected: %32b", snap.base);
         this.nErrors++;
       end
     end
