21d28f263144cfc0a3fa5b82483357e881ef67b0
Atila Romero <atilaromero@gmail.com>
2018-06-13 18:58:26 -0300

    WIP debug_address

---
+++ hf-riscv/verif/callbacks/monitor/debug_address.sv
@@ -0,0 +1,11 @@
+class Debug_address_cbs extends Monitor_cbs;
+
+  virtual task time_step(int t, ref Timemachine timemachine);
+    Snapshot snap;
+    int i;
+    super.time_step(t, timemachine);
+    snap = timemachine.snapshot[t];
+
+    $display("address: %8h", snap.address);
+  endtask
+endclass
