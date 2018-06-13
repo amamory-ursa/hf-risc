ddf9ce1eb03894ac865ade62d832f32888a909a3
Atila Romero <atilaromero@gmail.com>
2018-06-13 16:59:03 -0300

    WIP assert_addi

--- hf-riscv/verif/tb_top.sv
+++ hf-riscv/verif/tb_top.sv
@@ -12,7 +12,7 @@ module tb_top;
 `include "environment.sv"
 `include "callbacks/monitor/cover_instructions.sv"
 `include "callbacks/monitor/cover_opcodes.sv"
-// `include "callbacks/monitor/assert_addi.sv"
+`include "callbacks/monitor/assert_addi.sv"
 // `include "callbacks/monitor/assert_lui.sv"
 `include "callbacks/monitor/debug_instruction.sv"
 `include "callbacks/monitor/debug_registers.sv"
@@ -31,14 +31,14 @@ module tb_top;
       static environment env = new(iface);
       automatic CoverInstructions_cbs      cover_instructions_cbs = new;
       automatic CoverOpCodes_cbs           cover_opcodes_cbs = new;
-      // automatic Assert_addi_cbs            assert_addi_cbs = new(verbose);
+      automatic Assert_addi_cbs            assert_addi_cbs = new(verbose);
       // automatic Assert_lui_cbs             assert_lui_cbs = new(verbose);
       automatic Debug_instruction_cbs      debug_instruction_cbs = new;
       automatic Debug_registers_cbs        debug_registers_cbs = new;
       automatic Debug_uart                 debug_uart = new("sv_debug.txt");
       env.mon.cbs.push_back(cover_instructions_cbs);
       env.mon.cbs.push_back(cover_opcodes_cbs);
-      // env.mon.cbs.push_back(assert_addi_cbs);
+      env.mon.cbs.push_back(assert_addi_cbs);
       // env.mon.cbs.push_back(assert_lui_cbs);
       env.mon.cbs.push_back(debug_instruction_cbs);
       env.mon.cbs.push_back(debug_registers_cbs);
