f68851bedb3d8feb2028f32887114ae4d01b993c
Atila Romero <atilaromero@gmail.com>
2018-06-16 23:05:06 -0300

    rm callbacks/debug_address.sv

--- hf-riscv/verif/tb_top.sv
+++ hf-riscv/verif/tb_top.sv
@@ -15,7 +15,6 @@ module tb_top;
 `include "callbacks/assert_addi.sv"
 `include "callbacks/assert_lui.sv"
 `include "callbacks/debug_instruction.sv"
-`include "callbacks/debug_address.sv"
 `include "callbacks/debug_registers.sv"
 `include "callbacks/save_history.sv"
 `include "callbacks/debug_uart.sv"
@@ -36,7 +35,6 @@ module tb_top;
       automatic Assert_addi_cbs            assert_addi_cbs = new(verbose);
       automatic Assert_lui_cbs             assert_lui_cbs = new(verbose);
       automatic Debug_instruction_cbs      debug_instruction_cbs = new;
-      automatic Debug_address_cbs          debug_address_cbs = new;
       automatic Debug_registers_cbs        debug_registers_cbs = new;
       automatic Save_history_cbs           save_history_cbs = new("history.csv");
       automatic Debug_uart                 debug_uart = new("sv_debug.txt");
@@ -45,7 +43,6 @@ module tb_top;
       env.mon.cbs.push_back(assert_addi_cbs);
       env.mon.cbs.push_back(assert_lui_cbs);
       env.mon.cbs.push_back(debug_instruction_cbs);
-      // env.mon.cbs.push_back(debug_address_cbs);
       // env.mon.cbs.push_back(debug_registers_cbs);
       env.mon.cbs.push_back(save_history_cbs);
       env.mon.cbs.push_back(debug_uart);
