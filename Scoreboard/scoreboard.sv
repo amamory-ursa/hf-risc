994a554af3aa9d0a3af90d92eaf57bcc59f229cb
Paulo Vancin <paulo@gaphl02.portoalegre.pucrsnet.br>
2018-06-19 15:44:15 -0300

    Adding Scoreboard

--- Scoreboard/scoreboard.sv
+++ Scoreboard/scoreboard.sv
@@ -1,25 +1,69 @@
 import "DPI-C" function int run (input byte unsigned Mem2[4096], int size);
 import "DPI-C" context function void export_mem(input int);
+import "DPI-C" context function void send_pc();
+import "DPI-C" context function void send_cycles();
 export "DPI-C" function export_sram;
+export "DPI-C" function get_pc;
+export "DPI-C" function get_cycles;
 
+//---------------------------------------------------------------------
 
+typedef enum { in, out } direction;
+
+typedef struct {  
+  time t_time;
+  bit [7:0] value;
+  direction d;
+} trans_send_t;
 
 
 reg[31:0] Mem_from_ag[1024];
 reg[31:0] Mem_from_C[1024];
+reg[31:0] pc[521235];
+int cycles;
 mailbox m_box;
+mailbox m_box_io;
+mailbox m_box_pc;
+
+//--------------------RETREIVE-FROM-C-FUNCTIONS-------------------------
+
+function void get_cycles(input int _cycles);
+        cycles = _cycles;
+		$display("\nCycles:%d",cycles);
+endfunction
 
 
-
-function void export_sram(input reg[31:0] _sram[1024]);
-        for (int h=0;h<1024; h++)begin
-        Mem_from_C[h] = _sram[h];
+function void get_pc(input reg[31:0] _pc[521235]);
+        for (int h=0;h<521235; h++)begin
+			pc[h] = _pc[h];
         end
 endfunction
 
 
+function void export_sram(input reg[31:0] _sram[1024]);
+        for (int h=0;h<1024; h++)begin
+			Mem_from_C[h] = _sram[h];
+        end
+endfunction
+
+//--------------------------DUMMY-AGENT-IO------------------------------
+
+module dummy_ag_IO;
+
+ trans_send_t trans_send;
 
 
+	initial begin 
+		$display("Agent IO -> Data Acquired!!\n");
+		
+		m_box_io = new();
+		m_box_io.put(trans_send);		
+		
+	end		
+
+endmodule
+
+//--------------------------DUMMY-AGENT-MEMORY--------------------------
 
 module dummy_ag;
 
@@ -36,6 +80,7 @@ initial $readmemh("code.txt", Mem);
 
 endmodule
 
+//----------------------------SCOREBOARD--------------------------------
 
 module scoreboard;
 	int i, j, k, size;
@@ -43,12 +88,24 @@ module scoreboard;
 	reg [31:0] aux;
 	initial begin 
 	
+	trans_send_t trans_rec;
+	
 	$display("Scoreboard Initialized!!");
 	
 	  if(m_box)
-	  m_box.get(Mem_from_ag);
+		m_box.get(Mem_from_ag);
 	  else
-	  $display("ERROR - Mailbox Not Received !!!!!");
+		$display("ERROR - Mailbox Not Received !!!!!");
+	  
+	  if(m_box_io)
+		m_box_io.get(trans_rec);
+	  else
+		$display("ERROR - Mailbox 2 Not Received !!!!!");
+	  
+	  
+	  $display("Time:%d",trans_rec.t_time);
+	  $display("Value:%hhx",trans_rec.value);
+	  $display("Direction:%s",trans_rec.d);
 	  
 	  
 	  size = $size(Mem_byte);
@@ -66,28 +123,46 @@ module scoreboard;
 
 	    
 	export_mem(1024);
+	send_cycles();
+	send_pc();
 
 	m_box.put(Mem_from_C);
-	$display("Scoreboard Sent Mailbox!!");	
+	
+	m_box_pc = new();
+	m_box_pc.put(pc);
+	
+	$display("\nScoreboard Sent Mailboxes!!");	
 		
 	end		
 endmodule
+
+//-----------------------------DUMMY-CHECKER----------------------------
 
 
 module dummy_chk;
 
 reg[31:0] Memory_Out[1024];
+reg[31:0] PC_Out[521235];
 
 	initial begin 
 		$display("Data Acquired by Checker!!\n");
 		
       if(m_box)
-	  m_box.get(Memory_Out);
+		m_box.get(Memory_Out);
 	  else
-	  $display("ERROR - Mailbox Not Received !!!!!");	
+		$display("ERROR - Mailbox Not Received !!!!!");	
+	  
+	  if(m_box_pc)
+		m_box_pc.get(PC_Out);
+	  else
+		$display("ERROR - Mailbox Not Received !!!!!");
 	  
 	  for (int y=0; y<1024; y++)begin
-	  $display("Data [%d]:%hhx", y, Memory_Out[y]);
+		$display("Data [%d]:%hhx", y, Memory_Out[y]);
+      end
+      
+      for (int y=0; y<2000; y++)begin
+		$display("PC [%d]:%08x", y, PC_Out[y]);
       end
 	end		
 
--- Scoreboard/top.sv
+++ Scoreboard/top.sv
@@ -1,5 +1,6 @@
 module top;
 	dummy_ag ag1();
+	dummy_ag_IO ag2();
 	scoreboard sb1();
 	dummy_chk chk1();
 endmodule
