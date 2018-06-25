6dc0dda98f30785822d9fd2d06f9e838f4ef62c2
Alzemiro Lucas Silva (Aluno MSc - Moraes) <alzemiro@gmail.com>
2018-06-25 18:51:51 -0300

    added scripts for automated tests

---
+++ hf-riscv/verif/testcases/run_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+directory=$PWD
+args=""
+
+for d in */ ; do
+    echo "$d"
+    cd $directory/$d
+    args=""$d"coverage.ucdb $args"
+    ./run.sh
+done
+
+cd $directory
+vcover merge total_coverage.ucdb $args
+
