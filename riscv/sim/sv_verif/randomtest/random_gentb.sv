3af6b460a80147ab981074c87d38957d2ea7ea27
DarlanJurak <darlan.jurak@gmail.com>
2019-06-24 15:38:25 -0300

    corrected string format for apps files generation

--- riscv/sim/sv_verif/randomtest/random_gentb.sv
+++ riscv/sim/sv_verif/randomtest/random_gentb.sv
@@ -4,7 +4,7 @@
 `include "random_program.sv"
 
 
-`define NUMPROGRS 20
+`define NUMPROGRS 2
 
 module testbench;
 
@@ -19,7 +19,7 @@ module testbench;
     for (int i = 0; i < `NUMPROGRS; i++) begin
 
       //create a new randomized program
-      p = new (100); 
+      p = new (2); 
       program_code = p.toString();
       
       //display on screen
@@ -27,8 +27,8 @@ module testbench;
       $display(program_code);
 
       //save to file (note that these files are compiled elsewhere)
-      $sformat(filename, "apps/app%05d/app%05d.S", i, i);
-      $sformat(dirname,  "apps/app%05d", i);
+      $sformat(filename, "apps/app%0d/app%0d.S", i, i);
+      $sformat(dirname,  "apps/app%0d", i);
 
       $system({"mkdir ", dirname});
 
