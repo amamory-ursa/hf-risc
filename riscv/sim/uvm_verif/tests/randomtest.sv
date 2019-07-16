0f8fc72af9db57651872c05e83cb26094e0ff49b
Darlan Alves Jurak <darlan.jurak@gmail.com>
2019-07-16 11:00:50 -0300

    merge with verif_devel_darlan

--- riscv/sim/uvm_verif/tests/randomtest.sv
+++ riscv/sim/uvm_verif/tests/randomtest.sv
@@ -6,11 +6,15 @@ import uvm_pkg::*;
 `include "../uvm_src/randomtest/random_instruction.sv"
 `include "../uvm_src/randomtest/random_program.sv"
 
-`define NUM_PROGRS          3                       // Amount of constrained random programs to be created and simulated
-`define PROG_LENGTH         10                      // Amount of instructions in each constrained random program
-`define INSTRUCTS_TYPE      itype'(ITYPE)           // RTYPE, ITYPE, UTYPE,    STYPE,   BTYPE, JTYPE, NULL_TYPE
+`define NUM_PROGRS_RTYPE    2               // Amount of constrained random RTYPE programs to be created and simulated
+`define NUM_PROGRS_ITYPE    2               // Amount of constrained random ITYPE programs to be created and simulated
+`define NUM_PROGRS_STYPE    0               // Amount of constrained random STYPE programs to be created and simulated
+`define NUM_PROGRS_BTYPE    0               // Amount of constrained random BTYPE programs to be created and simulated
+`define NUM_PROGRS_UTYPE    0               // Amount of constrained random UTYPE programs to be created and simulated
+`define NUM_PROGRS_JTYPE    0               // Amount of constrained random JTYPE programs to be created and simulated
+`define PROG_LENGTH         100             // Amount of instructions in each constrained random program
+`define ITYPE_MEM_RANGE                     // For memomry write address range control
 `define INSTRUCTS_OPCODE    opcode'(NULL_OPCODE)    // ADD,   ...,   SRA, ..., LB, ..., ANDI,  ...,   JAL,   NULL_OPCODE
-`define ITYPE_MEM_RANGE     
 
 class randomtest extends uvm_test;
     `uvm_component_utils(randomtest)
@@ -59,14 +63,153 @@ class randomtest extends uvm_test;
     task run_phase(uvm_phase phase);
         super.run_phase(phase);
 
-        // while coverage_current_value < coverage_acceptance
-            // program_rand = new(prog_lentgh, coverage_next_priority_instr)
-        // end while
         phase.raise_objection(this);
-        for (i = 0; i < `NUM_PROGRS; i++) begin
+
+        // RTYPE
+        for (i = 0; i < `NUM_PROGRS_RTYPE; i++) begin
 
             //create a new randomized program
-            program_rand = new (`PROG_LENGTH, `INSTRUCTS_TYPE, `INSTRUCTS_OPCODE);
+            program_rand = new (`PROG_LENGTH, itype'(RTYPE), `INSTRUCTS_OPCODE);
+            program_code = program_rand.toString();
+
+            //display on screen
+            $display("-------------------------------");
+            $display(program_code);
+
+            //save to file
+            $sformat(filename, "randomtest/apps/app%0d/app%0d.S", i, i);    // Auxiliary variable for Assembly creation
+            $sformat(dirname,  "randomtest/apps/app%0d", i);                // Auxiliary variable for directory creation
+            $system({"mkdir ", dirname});                                   // Directory creation
+            f = $fopen(filename);                                           // Asssembly file creation
+            $fwrite(f, "%s", program_code);                                 // Asssembly creation
+            $fclose(f);                                                     // Asssembly file creation
+            $system({"cp randomtest/apps/build.sh ", dirname});             // Copy build script into app$d directory
+            $system({"(cd ", dirname, " ; bash build.sh)"});                // Build for binary creation
+            $system({"cp ", dirname, "/code.txt ./code.txt"});              // Copy binary file into simulaiton directory
+
+            // Connects the sequence to the generator inside the agent
+            seq.seqce = env.agt.seqcr;
+            seq.start(seq.seqce);
+            
+        end
+
+        // ITYPE
+        for (i = 0; i < `NUM_PROGRS_ITYPE; i++) begin
+
+            //create a new randomized program
+            program_rand = new (`PROG_LENGTH, itype'(ITYPE), `INSTRUCTS_OPCODE);
+            program_code = program_rand.toString();
+
+            //display on screen
+            $display("-------------------------------");
+            $display(program_code);
+
+            //save to file
+            $sformat(filename, "randomtest/apps/app%0d/app%0d.S", i, i);    // Auxiliary variable for Assembly creation
+            $sformat(dirname,  "randomtest/apps/app%0d", i);                // Auxiliary variable for directory creation
+            $system({"mkdir ", dirname});                                   // Directory creation
+            f = $fopen(filename);                                           // Asssembly file creation
+            $fwrite(f, "%s", program_code);                                 // Asssembly creation
+            $fclose(f);                                                     // Asssembly file creation
+            $system({"cp randomtest/apps/build.sh ", dirname});             // Copy build script into app$d directory
+            $system({"(cd ", dirname, " ; bash build.sh)"});                // Build for binary creation
+            $system({"cp ", dirname, "/code.txt ./code.txt"});              // Copy binary file into simulaiton directory
+
+            // Connects the sequence to the generator inside the agent
+            seq.seqce = env.agt.seqcr;
+            seq.start(seq.seqce);
+            
+        end
+
+        // STYPE
+        for (i = 0; i < `NUM_PROGRS_STYPE; i++) begin
+
+            //create a new randomized program
+            program_rand = new (`PROG_LENGTH, itype'(STYPE), `INSTRUCTS_OPCODE);
+            program_code = program_rand.toString();
+
+            //display on screen
+            $display("-------------------------------");
+            $display(program_code);
+
+            //save to file
+            $sformat(filename, "randomtest/apps/app%0d/app%0d.S", i, i);    // Auxiliary variable for Assembly creation
+            $sformat(dirname,  "randomtest/apps/app%0d", i);                // Auxiliary variable for directory creation
+            $system({"mkdir ", dirname});                                   // Directory creation
+            f = $fopen(filename);                                           // Asssembly file creation
+            $fwrite(f, "%s", program_code);                                 // Asssembly creation
+            $fclose(f);                                                     // Asssembly file creation
+            $system({"cp randomtest/apps/build.sh ", dirname});             // Copy build script into app$d directory
+            $system({"(cd ", dirname, " ; bash build.sh)"});                // Build for binary creation
+            $system({"cp ", dirname, "/code.txt ./code.txt"});              // Copy binary file into simulaiton directory
+
+            // Connects the sequence to the generator inside the agent
+            seq.seqce = env.agt.seqcr;
+            seq.start(seq.seqce);
+            
+        end
+
+        // UTYPE
+        for (i = 0; i < `NUM_PROGRS_UTYPE; i++) begin
+
+            //create a new randomized program
+            program_rand = new (`PROG_LENGTH, itype'(UTYPE), `INSTRUCTS_OPCODE);
+            program_code = program_rand.toString();
+
+            //display on screen
+            $display("-------------------------------");
+            $display(program_code);
+
+            //save to file
+            $sformat(filename, "randomtest/apps/app%0d/app%0d.S", i, i);    // Auxiliary variable for Assembly creation
+            $sformat(dirname,  "randomtest/apps/app%0d", i);                // Auxiliary variable for directory creation
+            $system({"mkdir ", dirname});                                   // Directory creation
+            f = $fopen(filename);                                           // Asssembly file creation
+            $fwrite(f, "%s", program_code);                                 // Asssembly creation
+            $fclose(f);                                                     // Asssembly file creation
+            $system({"cp randomtest/apps/build.sh ", dirname});             // Copy build script into app$d directory
+            $system({"(cd ", dirname, " ; bash build.sh)"});                // Build for binary creation
+            $system({"cp ", dirname, "/code.txt ./code.txt"});              // Copy binary file into simulaiton directory
+
+            // Connects the sequence to the generator inside the agent
+            seq.seqce = env.agt.seqcr;
+            seq.start(seq.seqce);
+            
+        end
+
+        // BTYPE
+        for (i = 0; i < `NUM_PROGRS_BTYPE; i++) begin
+
+            //create a new randomized program
+            program_rand = new (`PROG_LENGTH, itype'(BTYPE), `INSTRUCTS_OPCODE);
+            program_code = program_rand.toString();
+
+            //display on screen
+            $display("-------------------------------");
+            $display(program_code);
+
+            //save to file
+            $sformat(filename, "randomtest/apps/app%0d/app%0d.S", i, i);    // Auxiliary variable for Assembly creation
+            $sformat(dirname,  "randomtest/apps/app%0d", i);                // Auxiliary variable for directory creation
+            $system({"mkdir ", dirname});                                   // Directory creation
+            f = $fopen(filename);                                           // Asssembly file creation
+            $fwrite(f, "%s", program_code);                                 // Asssembly creation
+            $fclose(f);                                                     // Asssembly file creation
+            $system({"cp randomtest/apps/build.sh ", dirname});             // Copy build script into app$d directory
+            $system({"(cd ", dirname, " ; bash build.sh)"});                // Build for binary creation
+            $system({"cp ", dirname, "/code.txt ./code.txt"});              // Copy binary file into simulaiton directory
+
+            // Connects the sequence to the generator inside the agent
+            seq.seqce = env.agt.seqcr;
+            seq.start(seq.seqce);
+            
+        end
+
+        // JTYPE
+        for (i = 0; i < `NUM_PROGRS_JTYPE; i++) begin
+
+            //create a new randomized program
+            program_rand = new (`PROG_LENGTH, itype'(JTYPE), `INSTRUCTS_OPCODE);
             program_code = program_rand.toString();
 
             //display on screen
